daadbcc86439a7aeef6178fa0dd4cd19bfec25b9
Benoit Bonavia <benoit.bonavia@gmail.com>
2019-09-30 19:30:02 +0200

    ajout de l'environnement front et début d'ajout de font-awesome

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {HeaderComponent} from "./header/header.component";
+import {MatButtonModule} from "@angular/material/button";
+import {faPlus} from "@fortawesome/fontawesome-free";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
