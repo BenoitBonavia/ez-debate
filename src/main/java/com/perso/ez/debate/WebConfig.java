0a4a492ab6e8f23ff10d8e84f7c5b88fad95c221
Benoit <benoit.bonavia@gmail.com>
2019-10-09 14:05:42 +0200

    modification de la config

--- src/main/java/com/perso/ez/debate/WebConfig.java
+++ src/main/java/com/perso/ez/debate/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("", "/", "/**/*")
+        registry.addResourceHandler("/", "/", "/**/*")
                 .addResourceLocations("classpath:/static/")
                 .resourceChain(true)
                 .addResolver(new PathResourceResolver(){
