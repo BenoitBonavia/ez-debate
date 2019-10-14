9519855189bdb538690a5cc725485c280cc07023
Benoit <benoit.bonavia@gmail.com>
2019-10-14 15:17:16 +0200

    mise en place de l'édition du titre sur desktop

--- webapp/src/app/data/detail/detail.component.ts
+++ webapp/src/app/data/detail/detail.component.ts
@@ -10,7 +10,9 @@ import {DataModel} from "../../models/data.model";
 export class DetailComponent {
 
   data: DataModel;
+  tempData: DataModel;
   hoverArea: string = null;
+  editableArea: string = null;
 
   @ViewChild("verticalVideoCarouselContainer", {static: false}) verticalVideoCarouselContainer: ElementRef;
 
@@ -22,11 +24,30 @@ export class DetailComponent {
     this.activatedRoute.params.subscribe((params: ParamMap) => {
       this.dataService.getDataDetail(params['id']).subscribe(response => {
         this.data = response;
+        this.tempData = response;
       })
     })
   }
 
   setHover(val) {
     this.hoverArea = val;
+  }
+
+  setEditableArea(val) {
+    this.editableArea = val;
+    this.tempData = this.data;
+  }
+
+  cancel() {
+    this.editableArea = null;
+    this.tempData = this.data;
+  }
+
+  saveData() {
+    this.dataService.saveData(this.data).subscribe(response => {
+      this.data = response;
+      this.tempData = this.data;
+      this.editableArea = null;
+    })
   }
 }
