c3751403c84afc35a067e71c88603e71614dc821
Benoit <benoit.bonavia@gmail.com>
2019-10-26 16:39:02 +0200

    Avancement du formulaire d'inscription

--- webapp/src/app/security/register/register.component.ts
+++ webapp/src/app/security/register/register.component.ts
@@ -1,9 +1,19 @@
 import {Component, OnInit} from "@angular/core";
+import {FormControl, Validators} from "@angular/forms";
 
 @Component({
   selector: 'ed-register',
   templateUrl: 'register.component.html'
 })
 export class RegisterComponent {
-
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+  firstNameFormControl = new FormControl('', [
+    Validators.required
+  ]);
+  lastNameFormControl = new FormControl('', [
+    Validators.required
+  ]);
 }
