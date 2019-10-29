69567471976f0c98372f6e23aa8cc2049d7442d0
Benoit <benoit.bonavia@gmail.com>
2019-10-29 14:35:58 +0100

    control des formulaire fini avec le double champs password

--- webapp/src/app/security/register/register.component.ts
+++ webapp/src/app/security/register/register.component.ts
@@ -1,26 +1,39 @@
 import {Component, OnInit} from "@angular/core";
-import {FormControl, Validators} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'ed-register',
   templateUrl: 'register.component.html'
 })
-export class RegisterComponent {
-  emailFormControl = new FormControl('', [
-    Validators.required,
-    Validators.email,
-  ]);
-  firstNameFormControl = new FormControl('', [
-    Validators.required
-  ]);
-  lastNameFormControl = new FormControl('', [
-    Validators.required
-  ]);
-  passwordFormControl = new FormControl('', [
-    Validators.required,
-    Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')
-  ]);
-  confirmPasswordFormControl = new FormControl('', [
-    Validators.required,
-  ])
+export class RegisterComponent implements OnInit {
+
+  registerForm: FormGroup;
+
+  ngOnInit(): void {
+    this.registerForm = new FormGroup({
+      emailFormControl: new FormControl('', [
+        Validators.required,
+        Validators.email,
+      ]),
+      firstNameFormControl: new FormControl('', [
+        Validators.required
+      ]),
+      lastNameFormControl: new FormControl('', [
+        Validators.required
+      ]),
+      passwordFormControl: new FormControl('', [
+        Validators.required,
+        Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')
+      ]),
+      confirmPasswordFormControl: new FormControl('', [
+        Validators.required,
+      ])
+    }, passwordMatchValidator)
+  }
+}
+
+function passwordMatchValidator(g: FormGroup) {
+  console.log("bonjour");
+  console.log(g.get('passwordFormControl').value === g.get('confirmPasswordFormControl').value ? null : {'mismatch': true});
+  return g.get('passwordFormControl').value === g.get('confirmPasswordFormControl').value ? null : {'mismatch': true};
 }
