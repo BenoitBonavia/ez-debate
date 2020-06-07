7e56a5361b4a6ad7d82de03467fc63fd0b1304ca
benoit <benoit.bonavia@gmail.com>
2020-06-07 18:56:17 +0200

    suppression de la vieille authentification

--- src/main/java/com/perso/ez/debate/auth/VerificationTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.perso.ez.debate.auth;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface VerificationTokenRepository extends CrudRepository<VerificationToken, Long> {
-
-    VerificationToken findByToken(String token);
-    VerificationToken findByUser(UserEntity user);
-}
