6f43735b7a6ac2eb34ab6948f2008e59ae2573fa
Benoit <benoit.bonavia@gmail.com>
2020-06-29 20:56:26 +0200

    suppression de l'ancien moyen d'authentification

--- src/main/java/com/perso/ez/debate/EzDebateApplication.java
+++ src/main/java/com/perso/ez/debate/EzDebateApplication.java
@@ -1,7 +1,6 @@
 package com.perso.ez.debate;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
