31c43aee79ab42e3ec5b75cf23a1c36ba784693b
Benoit Bonavia <benoit.bonavia@gmail.com>
2020-08-17 10:09:32 +0200

    open ssl qui a l'air de marcher

--- src/main/java/com/perso/ez/debate/EzDebateApplication.java
+++ src/main/java/com/perso/ez/debate/EzDebateApplication.java
@@ -1,7 +1,14 @@
 package com.perso.ez.debate;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EzDebateApplication {
@@ -10,4 +17,31 @@ public class EzDebateApplication {
 		SpringApplication.run(EzDebateApplication.class, args);
 	}
 
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+
+		tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
+
+		return tomcat;
+	}
+
+	private Connector httpToHttpsRedirectConnector() {
+		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+		connector.setScheme("http");
+		connector.setPort(8082);
+		connector.setSecure(false);
+		connector.setRedirectPort(8080);
+		return connector;
+	}
 }
