ef9c5113b129d2857b4777c55221885eee347cf1
Benoit <benoit.bonavia@gmail.com>
2020-09-02 22:32:31 +0200

    test avec un proxy en prod

--- webapp/src/app/service/wayback-machine.service.ts
+++ webapp/src/app/service/wayback-machine.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from "@angular/core";
-import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable()
 export class WaybackMachineService {
@@ -13,6 +13,6 @@ export class WaybackMachineService {
   }
 
   getLinks(url) {
-    return this.httpClient.get('http://archive.org/wayback/available?url=' + url);
+    return this.httpClient.get('/waybackmachine/wayback/available?url==' + url);
   }
 }
