313d6f2fa17b8a537f4a4c915dc012c3157db990
Benoit <benoit.bonavia@gmail.com>
2020-09-07 18:39:31 +0200

    ajout du bouton de partage dans le carousel et reduction du nombre d'appel du reset des dimensions

--- webapp/src/app/common/media-carousel/media-carousel.component.ts
+++ webapp/src/app/common/media-carousel/media-carousel.component.ts
@@ -19,10 +19,13 @@ import {MediaModel} from "../../models/media.model";
 export class MediaCarouselComponent implements AfterViewChecked{
 
   @Input() medias: MediaModel[];
+  @Input() shareButton: boolean = true;
   @ViewChildren('element') elements: QueryList<ElementRef>;
   @ViewChild('container') container: ElementRef;
 
   elementsArray: ElementRef[];
+
+  hasToRecheck: boolean = true;
 
   currentElementIndex: number = 0;
   currentElement: ElementRef;
@@ -33,19 +36,30 @@ export class MediaCarouselComponent implements AfterViewChecked{
   leftMargin: number;
   rightMargin: number;
 
+  navigator = navigator;
+
   constructor(private cdRef: ChangeDetectorRef) {
 
   }
 
   ngAfterViewChecked() {
-    this.elementsArray = this.elements.toArray();
-    this.containerWidth = this.container.nativeElement.offsetWidth;
-    this.videoWidth = (this.container.nativeElement.offsetHeight/9)*16;
+    this.recheck();
+    if (this.hasToRecheck) {
+      console.log('reset');
+      this.elementsArray = this.elements.toArray();
+      this.containerWidth = this.container.nativeElement.offsetWidth;
+      this.videoWidth = (this.container.nativeElement.offsetHeight / 9) * 16;
 
-    this.leftMargin = (this.containerWidth - this.elementsArray[0].nativeElement.offsetWidth) / 2;
-    this.rightMargin = (this.containerWidth - this.elementsArray[this.elementsArray.length-1].nativeElement.offsetWidth) / 2;
+      this.leftMargin = Math.round((this.containerWidth - this.elementsArray[0].nativeElement.offsetWidth) / 2);
+      this.rightMargin = Math.round((this.containerWidth - this.elementsArray[this.elementsArray.length - 1].nativeElement.offsetWidth) / 2);
 
-    this.cdRef.detectChanges();
+      this.cdRef.detectChanges();
+    }
+  }
+
+  recheck() {
+    if (!this.hasToRecheck || !this.elementsArray) return;
+    this.hasToRecheck = !((this.leftMargin === (this.containerWidth - this.elementsArray[0].nativeElement.offsetWidth) / 2) && (this.rightMargin === (this.containerWidth - this.elementsArray[this.elementsArray.length - 1].nativeElement.offsetWidth) / 2));
   }
 
   next() {
@@ -61,5 +75,14 @@ export class MediaCarouselComponent implements AfterViewChecked{
       this.container.nativeElement.scrollLeft -= (this.elementsArray[this.currentElementIndex].nativeElement.offsetWidth + this.elementsArray[this.currentElementIndex - 1].nativeElement.offsetWidth)/2;
       this.currentElementIndex--;
     }
+  }
+
+  share(title, url) {
+    navigator.share({
+      title: title,
+      url: url
+    }).then(() => {
+
+    })
   }
 }
