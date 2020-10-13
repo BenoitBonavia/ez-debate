1b42efa9b584c9392d3ac7e4de2a25f7f28a9fa8
Benoit <benoit.bonavia@gmail.com>
2020-10-13 10:02:27 +0200

    ajout du filter dans la page de gestion des tags

--- webapp/src/app/screen/tags-screen/edit-tag/edit-tag.component.ts
+++ webapp/src/app/screen/tags-screen/edit-tag/edit-tag.component.ts
@@ -1,10 +1,13 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {TagModel} from "../../../models/tag.model";
 import {TagTypeModel} from "../../../models/tag-type.model";
 import {TagService} from "../../../service/tag.service";
 import {MatSnackBar} from "@angular/material/snack-bar";
 import {ConfirmationDialogConfigModel} from "../../../common/confirmation-dialog/confirmation-dialog-config.model";
 import {ConfirmationDialogComponent} from "../../../common/confirmation-dialog/confirmation-dialog.component";
+import {FormControl} from "@angular/forms";
+import {map, startWith} from "rxjs/operators";
+import {Observable, of} from "rxjs";
 
 @Component({
   selector: 'ed-edit-tag',
@@ -14,17 +17,30 @@ export class EditTagComponent {
 
   @Input() tagType: TagTypeModel = new TagTypeModel();
 
+  filterInput: FormControl = new FormControl();
+
   newTag: TagModel = new TagModel();
   editedTag: TagModel = new TagModel();
   allTags: TagModel[];
+  filteredTags: Observable<TagModel[]>;
 
-  constructor(private tagService: TagService, private matSnackBar: MatSnackBar, private confirmationDialog: ConfirmationDialogComponent) { }
+  constructor(private tagService: TagService, private matSnackBar: MatSnackBar, private confirmationDialog: ConfirmationDialogComponent) {
+    this.filteredTags = this.filterInput.valueChanges.pipe(
+      startWith(null),
+      map((tag: string) => tag ? this._filter(tag) : this.allTags.slice())
+    );
+  }
 
   ngOnInit(): void {
     this.tagService.getTagsByType(this.tagType).subscribe(response => {
       this.allTags = response;
-    })
+    });
     this.newTag.type = this.tagType;
+  }
+
+  private _filter(value): TagModel[] {
+    const lowerCaseValue = value.toLowerCase();
+    return this.allTags.filter(tag => tag.tag.toLowerCase().indexOf(lowerCaseValue) === 0)
   }
 
   filterTagsByType(tags: TagModel[], type: TagTypeModel) {
